a0aa49a0b4fc4722f3a1ff2aaeb14602c9d72382
mmaduatis <marc.maneduatis@cegedim.com>
2023-03-27 13:39:16 +0200

    Fix ad blocker preventing opening Formulari tab by removing button

--- src/formulari/Formulari.js
+++ src/formulari/Formulari.js
@@ -1,20 +1,11 @@
-import {Button, Stack} from "@mui/material";
-import {formUrl} from "../utils/Constants";
 
 const Formulari = () => {
 
-    const openForm = () => {
-        window.open(formUrl, '_blank');
-    }
-
     return (
         <div style={{marginLeft: '5%'}}>
-            <p className="text-description">Cliqueu el botó per obrir el formulari o, copieu i enganxeu el següent enllaç
+            <p className="text-description">Cliqueu el següent enllaç per anar al formulari o, copieu i enganxeu-lo
                 en una pestanya nova</p>
-            <Stack spacing={2} direction="row">
-                <Button onClick={openForm} variant="contained" color="secondary">Formulari</Button>
-            </Stack>
-            <br/>
+
             <a href='https://docs.google.com/forms/d/e/1FAIpQLSdpPIgZteiIjSit1B5jRcfALzFNqRcYxiKL2x84tAYi5-yjbg/viewform'
                target='_blank'
                style={{color: 'white', overflow: 'auto', wordWrap: 'break-word'}}
